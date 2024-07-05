a1812daa5749e3349951dff4f602ef66796258aa
Raghavb175 <raghavbhatia1705@outlook.com>
2024-07-05 13:19:02 -0600

    working

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Navbar } from './Compnents/Navbar'
 import './App.css'
 
 function App() {
- 
-
+  
   return (
     <>
+    <Navbar></Navbar>
     </>
   )
 }
