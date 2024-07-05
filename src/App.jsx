37022309f5d3c2f6bb365a8f0532421033ec22c6
Raghavb175 <raghavbhatia1705@outlook.com>
2024-07-05 14:04:11 -0600

    woking still

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Navbar } from './Compnents/Navbar'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
